06563119821cf7496f39ab614f8e3b19a404d6d8
Gabriel Loureiro <gabrieloureirof@gmail.com>
2019-11-04 22:49:12 -0300

    OpenGL 100%

--- work_part2/main.py
+++ work_part2/main.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     width, height = 800, 600
     app = QApplication(sys.argv)
     window = Window(width, height)
-    print ("Executando Projeto OpenGL... Por favor, aguarde a renderização.")
+    print ("\n##################################################################\n\t\t\tEQUIPE 01:\n\n>>> Executando Projeto OpenGL... Por favor, aguarde a renderização.\n\n##################################################################")
     window.show()
     sys.exit(app.exec_())
